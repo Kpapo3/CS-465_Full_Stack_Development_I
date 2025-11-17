d4e6c2e4af0579a025c5d3ed5ec5fa3e1584b9bb
Kpapo3 <kirapiiapo@gmail.com>
2025-11-16 20:38:36 -1000

    Module 3 Completed Baseline

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
+let trips = [];
+try {
+    const dataPath = path.join(__dirname, '..', '..', 'data', 'trips.json');
+    trips = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+} catch(err) {
+    console.error('Error reading JSON data:', err);
+    trips = [];
+}
+
 /* GET Travel View */
 const travel = (req, res) => {
-    res.render('travel', {title: 'Travlr Getaways'});
+    // 'trips' available in scope
+    res.render('travel', {title: 'Travlr Getaways', trips});
 };
 
 module.exports = {
